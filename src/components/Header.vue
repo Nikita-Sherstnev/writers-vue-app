09c63f2a62f99168d66d43632ed7d136d81552c4
Nikita-Sherstnev <sherstnyov@vivaldi.net>
2020-10-02 18:56:32 +0400

    Initial

---
+++ src/components/Header.vue
@@ -0,0 +1,7 @@
+<template>
+    <div id="nav">
+        <router-link to="/">Главная</router-link> |
+        <router-link to="/writers">Список писателей</router-link> |
+        <router-link to="/books">Список книг</router-link>
+    </div>
+</template>
